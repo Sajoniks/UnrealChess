bfd96bb20243124efd2cf2934b787bcfe74e5d80
Sajoniks <avesom33@mail.ru>
2020-11-17 23:20:19 +0300

    Removed unnecessary files

--- Source/UnrealChess/MoveRules.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MoveRules.h"
-
-FString MoveRules::GetPossibleMovesW(FString History, int64 WP, int64 WH, int64 WB, int64 WR, int64 WK, int64 WQ, int64 BP, int64 BH, int64 BB, int64 BR, int64 BK, int64 BQ)
-{
-	return FString();
-}
