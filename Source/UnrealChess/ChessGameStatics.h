977df4e0354b085cf83037d03072df08021e6f92
Sajoniks <avesom33@mail.ru>
2020-11-18 15:24:57 +0300

    Added chess static functions library

---
+++ Source/UnrealChess/ChessGameStatics.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ChessGameStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCHESS_API UChessGameStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
