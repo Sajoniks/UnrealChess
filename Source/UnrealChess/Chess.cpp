882662ad4fe4c905f6e19a5443026c09ab67fee4
Sajoniks <avesom33@mail.ru>
2020-11-20 15:38:17 +0300

    Basic chess piece actor

--- Source/UnrealChess/Chess.cpp
+++ Source/UnrealChess/Chess.cpp
@@ -2,20 +2,80 @@
 
 
 #include "Chess.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
 AChess::AChess()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
 
+	PieceBase = CreateDefaultSubobject<UStaticMeshComponent>("Base");
+	PieceTop = CreateDefaultSubobject<UStaticMeshComponent>("Top piece");
+
+	SetRootComponent(PieceBase);
+	PieceTop->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+
+	ConstructorHelpers::FObjectFinder<UDataTable> MeshFinder{ TEXT("DataTable'/Game/Blueprint/DT_ChessMeshAssets.DT_ChessMeshAssets'") };
+	ConstructorHelpers::FObjectFinder<UDataTable> MaterialFinder{ TEXT("DataTable'/Game/Blueprint/DT_ChessMaterialAssets.DT_ChessMaterialAssets'") };
+
+	if (MeshFinder.Succeeded() && MaterialFinder.Succeeded())
+	{
+		MeshPaths = MeshFinder.Object;
+		MaterialPaths = MaterialFinder.Object;
+
+		const auto Row = MeshPaths->FindRow<FChessMeshPath>("Base", "", false);
+		if (!Row)
+		{
+			UE_LOG(LogActor, Error, TEXT("Failed to set mesh for chess piece."))
+		}
+		else
+		{
+			PieceBase->SetStaticMesh(Row->Mesh);
+		}
+	}
+	else
+	{
+		UE_LOG(LogActor, Error, TEXT("Failed to found assets for chess piece. Check references in chess class."));
+	}
+	
+	ChessColor = EChessPieceColor::White;
+	ChessRole = EChessPieceRole::Pawn;
+
+	UpdateMesh();
+}
+
+void AChess::OnConstruction(const FTransform& Transform)
+{
+	UpdateMesh();
 }
 
 // Called when the game starts or when spawned
 void AChess::BeginPlay()
 {
 	Super::BeginPlay();
-	
+}
+
+void AChess::UpdateMesh()
+{
+	if (MaterialPaths && MeshPaths)
+	{
+		//Find mesh in DT
+		FName RoleName = UEnum::GetValueAsName(ChessRole);
+		const auto Row = MeshPaths->FindRow<FChessMeshPath>(RoleName, "", false);
+		if (Row)
+			PieceTop->SetStaticMesh(Row->Mesh);
+		
+		//Find material in DT
+		FName MaterialName = UEnum::GetValueAsName(ChessColor);
+		const auto MatRow = MaterialPaths->FindRow<FChessMaterialPath>(MaterialName, "", false);
+
+		if (MatRow)
+		{
+			PieceBase->SetMaterial(0, MatRow->Material);
+			PieceTop->SetMaterial(0, MatRow->Material);
+		}
+	}
 }
 
 // Called every frame
--- Source/UnrealChess/Chess.h
+++ Source/UnrealChess/Chess.h
@@ -4,20 +4,83 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+
+
 #include "Chess.generated.h"
+
+class UDataTable;
+
+USTRUCT(BlueprintType)
+struct FChessMeshPath : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* Mesh;
+};
+
+USTRUCT(BlueprintType)
+struct FChessMaterialPath : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterialInstance* Material;
+};
+
+UENUM(BlueprintType)
+enum class EChessPieceRole : uint8
+{
+	Pawn,
+	Bishop,
+	Knight,
+	Rook,
+	Queen,
+	King
+};
+
+UENUM(BlueprintType)
+enum class EChessPieceColor : uint8
+{
+	Black,
+	White
+};
 
 UCLASS()
 class UNREALCHESS_API AChess : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY()
+	UDataTable* MeshPaths;
+
+	UPROPERTY()
+	UDataTable* MaterialPaths;
 	
 public:	
 	// Sets default values for this actor's properties
 	AChess();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Appearence")
+	EChessPieceRole ChessRole;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Appearence")
+	EChessPieceColor ChessColor;
+
+	void OnConstruction(const FTransform& Transform) override;
+
 protected:
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, Instanced, Category="Mesh")
+	UStaticMeshComponent* PieceBase;
+
+	UPROPERTY(VisibleDefaultsOnly, Instanced, Category = "Mesh")
+	UStaticMeshComponent* PieceTop;
+
+	void UpdateMesh();
 
 public:	
 	// Called every frame
